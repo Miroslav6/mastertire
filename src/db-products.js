2d8f7d7027b9c45a2c936297239aa31ea3171caa
Miroslav6 <miroslav.nedev1@gmail.com>
2021-12-29 22:41:57 +0200

    Work on Products page
    Work on Products page

---
+++ src/db-products.js
@@ -0,0 +1,28 @@
+export const productData = [
+    {
+        "id": 1,
+        "brand":"Dunlop",
+        "model": "Sportmax D213",
+        "age":30,
+        "image": "../Files/Images/Products/D2013-GP-PRO.png"
+    },
+    {
+        "id": 2,
+        "brand":"Pireli",
+        "model":"Superbike",
+        "age":30
+    },
+    {
+        "id": 3,
+        "brand":"Dunlop",
+        "model": "Sportmax D213",
+        "age":30,
+        "image": ""
+    },
+    {
+        "id": 4,
+        "brand":"Pireli",
+        "model":"Superbike",
+        "age":30
+    }
+]
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
+import { useEffect } from 'react';
+import {productData} from "../db-products";
 
 
+export const Products = () => {
 
-function Products() {
-
-    const backendData = [
-      { title: "Grocery List", description: "Milk, Soup, Bread", createdat: "01-18-2021" },
-      { title: "Math Homework", description: "Remember to finish question 8-10 before monday", createdat: "12-01-2020" },
-      { title: "Call James", description: "Ask him about the company party.", createdat: "12-30-2020" }
-    ];
-  console.log(backendData);
+  const backendData = [
+    { title: "Grocery List", description: "Milk, Soup, Bread", createdat: "01-18-2021" },
+    { title: "Math Homework", description: "Remember to finish question 8-10 before monday", createdat: "12-01-2020" },
+    { title: "Call James", description: "Ask him about the company party.", createdat: "12-30-2020" }
+  ];
   return (
-    <div className="header">
-      <h1>Products</h1>
-    </div>
+    <>
+      <div>
+        <h1>Products</h1>
+
+        <div className="row stock-container">
+          {productData.map((data, key) => {
+            return (
+              <div className='col col-sm-3' key={key}>
+                <div><img src={data.image} alt="Logo" /></div>
+                <div>{data.brand}</div>
+                <div>{data.model}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   )
 }
 
