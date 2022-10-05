bbc66fbd48966571b45d7a637e8ebb85cde7a440
Miroslav6 <miroslav.nedev1@gmail.com>
2022-10-05 08:14:55 +0300

    master
    added new dependencies

--- src/index.js
+++ src/index.js
@@ -20,8 +20,24 @@ import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bulma/css/bulma.css';
 import './assets/css/main.scss';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from "@apollo/client"
+import { onError } from "@apollo/client/link/error"
 
-
+const errorLink = onError(({graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path}) => {
+      alert(`Graphql error ${message}`)
+    })
+  }
+})
+const link = from ([
+  errorLink,
+  new HttpLink({uri: "http://localhost:3000/graphql"})
+])
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link
+})
 ReactDOM.render(
   <><React.StrictMode>
 
